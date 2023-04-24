7e67fec12667e56bf071eb715de9e7ce6552044a
Mehmet Ali Ulutaş <103904368+mehmet1101@users.noreply.github.com>
2023-04-24 10:18:58 +0300

    24 nisan

---
+++ index.js
@@ -0,0 +1,89 @@
+// Aşağıdaki görevleri tamamlayın
+
+/*
+  GÖREV 1
+  - Input:  Bir dosya yolunu (path) parametre olarak alacak bir fonksiyon oluştur.
+  - Output: Yazacağın fonksiyon, verilen bir dosya adresideki(path) dosyanın adını vermeli.
+  * İpucu:  Dosya adresinin / işaretleri ile bölümlendiğine dikkat et.
+
+  örnek input:  "C:/Users/johnsmith/Music/Beethoven_5.mp3"
+  örnek output: "Beethoven_5.mp3"
+
+  örnek input:  "Beethoven_5.mp3"
+  örnek output: "Beethoven_5.mp3"
+
+  örnek input:  ""
+  örnek output: ""
+*/
+
+function dosyaAdiniBul(dosyaYolu) {
+  const dosyaAdi = dosyaYolu.split('/').pop();
+  return dosyaAdi;
+  }
+  console.log(dosyaAdiniBul("Beethoven_5.mp3"));
+
+/*
+  GÖREV 2
+  - Input:  Bir sayı arrayini parametre olarak alacak bir fonksiyon oluştur.
+  - Output: Yazacağın fonksiyon input olarak aldığı sayı arrayinin aritmetik ortalamasını dönmeli.
+
+  örnek input:  []
+  örnek output: null
+
+  örnek input:  [4]
+  örnek output: 4
+  
+  örnek input:  [50, -26, 153, 7]
+  örnek output: 46
+
+  örnek input:  [109, 216, 288, 143, 71, 185, -278, 194, 5]
+  örnek output: 104
+*/
+function ortalamaBul(sayilar) {
+  if (sayilar.length === 0) {
+    return null;
+  }
+  const toplam = sayilar.reduce((previous, current) => current += previous);
+  return toplam / sayilar.length;
+  
+}
+
+/*
+  GÖREV 3
+  - Input:  Bir sayı arrayini ve ortalama bulmaya yarayacak bir fonksiyonu parametre olarak alacak bir fonksiyon oluştur.
+  - Output: Yazacağın fonksiyon input olarak aldığı sayı arrayinin aritmetik ortalamasından büyük ya da eşit elemanlardan oluşan bir array dönmeli.
+  * Çağırırken kendi ortalama fonksiyonunla çağırabilirsin, ama parametrenin tanımlama aşamasında soyut bir ifade olduğunu unutma.
+  * f(x) = 2x+5 içerisindeki x değeri gibi.
+
+  örnek input:  []
+  örnek output: null
+
+  örnek input:  [4]
+  örnek output: 4
+  
+  örnek input:  [50, -26, 153, 7]
+  örnek output: [50, 153]
+
+  örnek input:  [109, 216, 288, 143, 71, 185, -278, 194, 5]
+  örnek output: [109, 216, 288, 143, 185, 194]
+*/
+
+function buyukEsitOrtalama(arr, ortalamaBulFn) {
+  return arr.filter((num) => num >= ortalamaBulFn(arr));
+}
+
+// Örnek Kullanım
+function ortalamadanBuyukleriBul(sayilar) {
+  const toplam = sayilar.reduce((previous, current) => current += previous);
+  return toplam / sayilar.length;
+}
+const array = [10, 20, 30, 40, 50, 60];
+console.log(buyukEsitOrtalama(array, ortalamadanBuyukleriBul));
+
+/* !!!! Burdan aşağısını değiştirmeyin !!!! */
+function as() {
+  console.log("Kodlar sorunsuz çalışıyor!");
+  return "sa";
+}
+as();
+export { dosyaAdiniBul, ortalamaBul, ortalamadanBuyukleriBul, as };
